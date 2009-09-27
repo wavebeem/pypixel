8323419abf50303c73026ad650a48b8fc587938e
Brian Mock <mock.brian@gmail.com>
2009-09-27 16:45:49 +0000

    Becoming quite feature complete. Many examples in the main.py file.

--- main.py
+++ main.py
@@ -2,34 +2,65 @@
 
 from pypixel import *
 
-hue = 0
-def next_color():
-    global hue
-    hue %= 360
-    c = Color(0)
-    c.hsva = (hue, 100, 100, 100)
-    hue += 1
-    return c
-
 def circles():
-    size = 100
+    hue = 0
+    size = 10
     while True:
         for x in xrange(0, WIDTH + 1, size):
             for y in xrange(0, HEIGHT + 1, size):
-                circle(next_color(), (x, y), size)
+                circle(hsv((hue, 100, 100)), (x, y), size)
+                hue += 1
+                hue %= 360
+            update()
 
 def lines():
+    hue = 0
     width = 1
     for x in xrange(0, WIDTH + 1, width):
-        line(next_color(), (x, 0), (x, HEIGHT), width)
+        line(hsv((hue, 100, 100)), (x, 0), (x, HEIGHT), width)
+        update()
+        hue += 1
     for x in reversed(xrange(0, WIDTH + 1, 1)):
-        line(next_color(), (x, 0), (x, HEIGHT), width)
+        line(hsv((hue, 100, 100)), (x, 0), (x, HEIGHT), width)
+        update()
+        hue += 1
+        hue %= 360
 
 def transition():
+    hue = 0
     size = WIDTH
     while True:
         for x in xrange(0, WIDTH + 1, size):
             for y in xrange(0, HEIGHT + 1, size):
-                circle(next_color(), (x, y), size)
+                circle(hsv((hue, 100, 100)), (x, y), size)
+                update()
+                hue += 1
+                hue %= 360
 
-run(transition)
+def random_circles():
+    while True:
+        x = random(WIDTH)
+        y = random(HEIGHT)
+        r = random(1, 100)
+        h = random(360)
+        s = 100
+        v = 100
+        c = hsv((h, s, v))
+        circle(c, (x, y), r)
+        update()
+
+def rainbow_random_circles():
+    h = 0
+    while True:
+        x  = random(WIDTH)
+        y  = random(HEIGHT)
+        r  = random(1, 100)
+        h += 1
+        h %= 360
+        s  = 100
+        v  = 100
+        c  = hsv((h, s, v))
+        circle(c, (x, y), r)
+        update()
+
+run(rainbow_random_circles)
--- pypixel.py
+++ pypixel.py
@@ -4,44 +4,99 @@
 # Thread pypixel so that the end user doesn't have to insert check()
 # everywhere...
 #
+# TODO
+# Write color wrapper for HSVA, HSLA, RGBA
+#
 
+import  random  as randy
 import  pygame
 from    pygame.locals   import *
 
+# Screen size
 WIDTH  = 640
 HEIGHT = 480
 
-# Copy over the pygame color class
-Color = pygame.Color
+def hsv(hsv):
+    '''Create a new color from an HSV triplet'''
+    c = pygame.Color(0)
+    c.hsva = hsv + (100,) 
+    return c
+
+def hsva(hsv):
+    '''Create a new color from an HSVA quadruplet'''
+    c = pygame.Color(0)
+    c.hsva = hsva
+    return c
+
+def hsl(hsl):
+    '''Create a new color from an HSL triplet'''
+    c = pygame.Color(0)
+    c.hsla = hsl + (100,) 
+    return c
+
+def hsla(hsl):
+    '''Create a new color from an HSLA quadruplet'''
+    c = pygame.Color(0)
+    c.hsla = hsla
+    return c
+
+def rgb(rgb):
+    '''Create a new color from an RGB triplet'''
+    c = pygame.Color(*rgb)
+    return c
+
+def rgba(rgba):
+    '''Create a new color from an RGBA quadruplet'''
+    c = pygame.Color(*rgba)
+    return c
+
+def check():
+    '''Check to see if the user wants to quit.
+       That is, if they pressed Q, Esc, or tried to close the window.'''
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            print "Got signal to quit"
+            exit()
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                print "Got signal to quit"
+                exit()
+            elif event.key == K_q:
+                print "Got signal to quit"
+                exit()
 
 def run(main):
-    '''Set up the basic pypixel environment'''
+    '''Set up the basic pypixel environment and run the main function'''
     pygame.init()
     pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.mouse.set_visible(False)
 
     main()
-    while True:
-        check()
-
-def check():
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            exit()
-        if event.type == KEYDOWN:
-            if event.key == K_ESCAPE:
-                exit()
-            elif event.key == K_q:
-                exit()
-
 
 def screen():
     return pygame.display.get_surface()
 
-def line(color, start, end, width=1):
-    pygame.draw.line(screen(), color, start, end, width)
+def update():
+    '''Update the display and check if the user wants to quit'''
     pygame.display.update()
+    check()
 
-def circle(color, center, radius):
-    pygame.draw.circle(screen(), color, center, radius, 0)
-    pygame.display.update()
+def line(color, start, end, width=1):
+    '''Draw a line on the screen.
+       color, start, end, [width]
+       If width is not given, make it 1px wide.'''
+    pygame.draw.line(screen(), color, start, end, width)
+
+def circle(color, center, radius, width=0):
+    '''Draw a circle on the screen.
+       color, center, radius, [width]
+       if width is 0 or not given, fill in the circle.'''
+    pygame.draw.circle(screen(), color, center, radius, width)
+
+def random(x=None, y=None):
+    if x is None and y is None:
+        return randy.random()
+    elif y is None:
+        return randy.randrange(0, x)
+    else:
+        return randy.randrange(x, y)
