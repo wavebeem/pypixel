60a86b8ad4124eec0aa3ffbc2204698512fdd784
Brian Mock <mock.brian@gmail.com>
2009-11-16 07:00:53 +0000

    pix.py now does not go on forever.

--- examples/pix.py
+++ examples/pix.py
@@ -3,10 +3,8 @@
 from pypixel import *
 show()
 
-while True:
-    for i in xrange(100):
-        x = random(0, WIDTH)
-        y = random(0, HEIGHT)
-        pixel((x, y), WHITE)
-        update()
-    clear()
+for i in xrange(100):
+    x = random(0, WIDTH)
+    y = random(0, HEIGHT)
+    pixel((x, y), WHITE)
+    update()
