341bff1b7b111ec3b78f1957c066434cb736bfde
Brian Mock <mock.brian@gmail.com>
2010-03-07 01:23:24 +0000

    Added a sierpinski triangle demo program

---
+++ examples/sierpinski.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+from pypixel import *
+
+title("Sierpinski demo")
+show()
+
+vertices = [
+    (WIDTH/2,        0), # Top
+    (      0,   HEIGHT), # Bottom left
+    (  WIDTH,   HEIGHT)  # Bottom right
+]
+
+point = CENTER
+
+step_size = 250
+
+while True:
+    for i in xrange(step_size):
+        vertex = vertices[random(0, 2)]
+        point  = midpoint(vertex, point)
+        pixel(YELLOW, point)
+    update()
