42821187846b2a345edc9ed8241564c8790862db
Brian Mock <mock.brian@gmail.com>
2011-02-27 18:16:25 -0800

    Made circles.py more readable

--- examples/circles.py
+++ examples/circles.py
@@ -8,7 +8,10 @@ hue  = 0
 size = 10
 for x in xrange(0, WIDTH + 1, size):
     for y in xrange(0, HEIGHT + 1, size):
-        circle(hsv2rgb((hue, 100, 100)), (x, y), size)
+        hsv = (hue, 100, 100)
+        color = hsv2rgb(hsv)
+        position = (x, y)
+        circle(color, position, size)
         hue += 1
         hue %= 360
     update()
