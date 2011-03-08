dde6df1e5d7aeafd3b566b93a7deb0a4b470cf54
Brian Mock <mock.brian@gmail.com>
2011-03-08 15:29:59 -0800

    Added licensing details

--- pypixel.py
+++ pypixel.py
@@ -1,10 +1,24 @@
+### pypixel - Python educational graphics library
+### Copyright 2009-2011 Brian Mock <mock.brian@gmail.com>
+###
+### This library is free software; you can redistribute it and/or
+### modify it under the terms of the GNU Library General Public
+### License as published by the Free Software Foundation; either
+### version 2 of the License, or (at your option) any later version.
+###
+### This library is distributed in the hope that it will be useful,
+### but WITHOUT ANY WARRANTY; without even the implied warranty of
+### MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+### Library General Public License for more details.
+###
+### You should have received a copy of the GNU Library General Public
+### License along with this library; if not, write to the Free
+### Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 '''
 pypixel is a simple graphics library intended for educational use.
 It is a collection of verbosely named functions that achieve basic geometric shape drawing.
 '''
-
-# TODO
-# - Wrap Python functions so we can have standard documentation style?
 
 __author__  = "Brian Mock <mock.brian@gmail.com>"
 __version__ = "0.1"
